381c4d97595b9776d59c0c716e634cedcc4437ee
RhinoLee <picklerhino@gmail.com>
2023-12-10 19:10:13 +0800

    header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from "@assets/icon/logo.svg";
+</script>
 
 <template>
   <div class="border border-gray-1 h-[72px]">
     <div class="container flex items-center m-auto h-full">
-      <!-- left -->
-      <h1 class="text-black-1 font-bold text-[2rem]">
+      <h1 class="flex items-center text-black-1 font-bold text-[2rem]">
+        <a href="/"><img :src="Logo.src" alt="logo" /></a>
         <a href="/">ToDoList</a>
       </h1>
-      <!-- center -->
-      <!-- right -->
     </div>
   </div>
 </template>

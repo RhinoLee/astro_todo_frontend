8ddd176d4d87300742f09b3fbde951991210011e
RhinoLee <picklerhino@gmail.com>
2023-12-11 00:39:45 +0800

    fix api base url error (condition)

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const isProd = import.meta.env.PROD;
-const isDev = import.meta.env.DEV;
-
-let BASE_URL = "";
-if (isDev) BASE_URL = import.meta.env.PUBLIC_BACKEND_API;
-else if (isProd) BASE_URL = import.meta.env.DB_BACKEND_API;
+const BASE_URL =
+  import.meta.env.DB_BACKEND_API || import.meta.env.PUBLIC_BACKEND_API;
 
 const $axios = axios.create({
   baseURL: BASE_URL,

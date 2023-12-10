fad250ceb8b99d9b638e04da32a69559154b83a7
RhinoLee <picklerhino@gmail.com>
2023-12-10 15:25:08 +0800

    base layout & component style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>Footer</h2>
+  <div class="flex justify-center items-center h-[64px] bg-black-1 text-white">
+    <h2 class="text-[1.3rem]">Copyright © Rhino Lee 2023</h2>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>Header</h2>
+  <div class="border border-gray-1 h-[72px]">
+    <div class="container flex items-center m-auto h-full">
+      <!-- left -->
+      <h1 class="text-black-1 font-bold text-[2rem]">
+        <a href="/">ToDoList</a>
+      </h1>
+      <!-- center -->
+      <!-- right -->
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
